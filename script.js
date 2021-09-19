53e066ac3d1c4a1f7d15478b6982a2b3ead810e8
SheikhRashed <devrashed06@gmail.com>
2021-09-19 20:33:01 +0600

    add random user event created

--- script.js
+++ script.js
@@ -54,3 +54,7 @@ function updateDOM(providedData = data) {
 function formatMoney(number) {
   return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+console.log(data);
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
